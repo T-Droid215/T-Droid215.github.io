7ab782754cac6dfa13a990c48e6ee51c75ea5f02
T-Droid215 <52387874+T-Droid215@users.noreply.github.com>
2025-02-25 12:42:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const introText = "Welcome to the empire...\n> Assembling the greeters...\n> Gates opening!";
+let i = 0;
+const speed = 50;
+
+function typeEffect() {
+    if (i < introText.length) {
+        let char = introText.charAt(i);
+
+        if (char === "\n") {
+            document.getElementById("intro-text").innerHTML += "<br>";
+        }   else {
+            document.getElementById("intro-text").innerHTML += char;
+        }
+
+        i++;
+        setTimeout{typeEffect, speed};
+    }
+}
+
+document.addEventListener("DOMContentLoeaded", typeEffect);
